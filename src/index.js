b0c805b777b023b8cde8a1ad9616637560ea18f6
Emerson Broga <emerson@emersonbroga.com>
2020-06-14 09:14:47 -0300

    Added sass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
