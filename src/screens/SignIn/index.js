0e81da0d2e5aa2df1cd730200b5968a84f109781
Emerson Broga <emerson@emersonbroga.com>
2020-06-17 21:20:44 -0300

    Redux added

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signIn } from './SignInActions';
 
-const SignIn = () => {
+const SignIn = (props) => {
+  const { account, signIn } = props;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    signIn({ email: 'emerson@emersonbroga.com', password: '123456' });
+  };
+
+  console.log('*** SignIn.account', account);
+
   return (
     <div className="container h-100 pt-5">
       <h1>Sign In</h1>
       <div className="d-flex flex-column h-100">
-        <form>
+        <form onSubmit={submitHandler}>
           <div className="form-group">
             <label>Email</label>
             <input type="text" className="form-control" />
@@ -28,4 +40,8 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+const mapStateToProps = (state) => {
+  return { account: state.signIn.account };
+};
+
+export default connect(mapStateToProps, { signIn })(SignIn);
